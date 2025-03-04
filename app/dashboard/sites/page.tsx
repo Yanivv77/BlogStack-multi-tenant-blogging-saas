170f3ad0e24192b8a47bea67845fe3ea83fb9f24
Yaniv Bialik <yanivv77@gmail.com>
2025-03-04 20:39:03 +0200

    implement site data fetching and display logic in SitesRoute

--- app/dashboard/sites/page.tsx
+++ app/dashboard/sites/page.tsx
@@ -1,9 +1,32 @@
+import prisma from "@/app/utils/db";
 import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import {  PlusCircle } from "lucide-react";
+import { FileIcon, PlusCircle } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
-import {  redirect } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
+import Defaultimage from "@/public/default.png";
+import { EmptyState } from "@/app/components/dashboard/EmptyState";
 
+async function getData(userId: string) {
+  const data = await prisma.site.findMany({
+    where: {
+      userId: userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return data;
+}
 
 export default async function SitesRoute() {
   const { getUser } = getKindeServerSession();
@@ -12,7 +35,7 @@ export default async function SitesRoute() {
   if (!user) {
     return redirect("/api/auth/login");
   }
-
+  const data = await getData(user.id);
   return (
     <>
       <div className="flex w-full justify-end">
@@ -23,9 +46,43 @@ export default async function SitesRoute() {
         </Button>
       </div>
 
+      {data === undefined || data.length === 0 ? (
+        <EmptyState
+          title="You dont have any Sites created"
+          description="You currently dont have any Sites. Please create some so that you can
+        see them right here!"
+          buttonText="Create Site"
+          href="/dashboard/sites/new"
+        />
+      ) : (
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 lg:gap-7">
+          {data.map((item) => (
+            <Card key={item.id}>
+              <Image
+                src={item.imageUrl ?? Defaultimage}
+                alt={item.name}
+                className="rounded-t-lg object-cover w-full h-[200px]"
+                width={400}
+                height={200}
+              />
+              <CardHeader>
+                <CardTitle className="truncate">{item.name}</CardTitle>
+                <CardDescription className="line-clamp-3">
+                  {item.description}
+                </CardDescription>
+              </CardHeader>
+
+              <CardFooter>
+                <Button asChild className="w-full">
+                  <Link href={`/dashboard/sites/${item.id}`}>
+                    View Articles
+                  </Link>
+                </Button>
+              </CardFooter>
+            </Card>
+          ))}
         </div>
-    
+      )}
     </>
   );
 }
