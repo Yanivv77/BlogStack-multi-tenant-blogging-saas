d9cdffe53d812e44a0770c0563ce9511766d87b0
Yaniv Bialik <yanivv77@gmail.com>
2025-03-04 21:21:12 +0200

    add SettingsSiteRoute

--- app/actions.ts
+++ app/actions.ts
@@ -3,7 +3,7 @@
 import { parseWithZod } from "@conform-to/zod";
 import {getKindeServerSession} from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
-import { SiteCreationSchema, siteSchema } from "./utils/zodSchemas";
+import { PostSchema, SiteCreationSchema, siteSchema } from "./utils/zodSchemas";
 import prisma from "./utils/db";
 import { requireUser } from "./utils/requireUser";
 
@@ -34,4 +34,101 @@ export async function CreateSiteAction(prevState: any, formData: FormData) {
 
 return redirect("/dashboard/sites");
  
+}
+
+
+export async function CreatePostAction(prevState: any, formData: FormData) {
+  const user = await requireUser();
+
+  const submission = parseWithZod(formData, {
+    schema: PostSchema,
+  });
+
+  if (submission.status !== "success") {
+    return submission.reply();
+  }
+
+  const data = await prisma.post.create({
+    data: {
+      title: submission.value.title,
+      smallDescription: submission.value.smallDescription,
+      slug: submission.value.slug,
+      articleContent: JSON.parse(submission.value.articleContent),
+      image: submission.value.coverImage,
+      userId: user.id,
+      siteId: formData.get("siteId") as string,
+    },
+  });
+
+  return redirect(`/dashboard/sites/${formData.get("siteId")}`);
+}
+
+export async function EditPostActions(prevState: any, formData: FormData) {
+  const user = await requireUser();
+
+  const submission = parseWithZod(formData, {
+    schema: PostSchema,
+  });
+
+  if (submission.status !== "success") {
+    return submission.reply();
+  }
+
+  const data = await prisma.post.update({
+    where: {
+      userId: user.id,
+      id: formData.get("articleId") as string,
+    },
+    data: {
+      title: submission.value.title,
+      smallDescription: submission.value.smallDescription,
+      slug: submission.value.slug,
+      articleContent: JSON.parse(submission.value.articleContent),
+      image: submission.value.coverImage,
+    },
+  });
+
+  return redirect(`/dashboard/sites/${formData.get("siteId")}`);
+}
+
+export async function DeletePost(formData: FormData) {
+  const user = await requireUser();
+
+  const data = await prisma.post.delete({
+    where: {
+      userId: user.id,
+      id: formData.get("articleId") as string,
+    },
+  });
+
+  return redirect(`/dashboard/sites/${formData.get("siteId")}`);
+}
+
+export async function UpdateImage(formData: FormData) {
+  const user = await requireUser();
+
+  const data = await prisma.site.update({
+    where: {
+      userId: user.id,
+      id: formData.get("siteId") as string,
+    },
+    data: {
+      imageUrl: formData.get("imageUrl") as string,
+    },
+  });
+
+  return redirect(`/dashboard/sites/${formData.get("siteId")}`);
+}
+
+export async function DeleteSite(formData: FormData) {
+  const user = await requireUser();
+
+  const data = await prisma.site.delete({
+    where: {
+      userId: user.id,
+      id: formData.get("siteId") as string,
+    },
+  });
+
+  return redirect("/dashboard/sites");
 }
