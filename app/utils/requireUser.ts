af65968cd4ebff21fb1fd246dfc46bef697ecbe0
Yaniv Bialik <yanivv77@gmail.com>
2025-03-10 00:20:25 +0200

    fix redirect unauthenticated users to login page in requireUser utility

--- app/utils/requireUser.ts
+++ app/utils/requireUser.ts
@@ -1,11 +1,12 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
 
 export const requireUser = async () => {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
 
   if (!user) {
-    return null;
+    return redirect("/api/auth/login");
   }
 
   return user;
